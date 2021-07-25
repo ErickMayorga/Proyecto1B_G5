d608184290d48118b605c606f40613ff48fa2b4e
Davidaguirre99 <Davidaguirre99@users.noreply.github.com>
2021-07-25 14:12:23 -0500

    Se implementa de manera adecuada el metodo mostrar Materias Disponibles

--- src/main/java/modulo/matriculacion/VisualizadorMatriculas.java
+++ src/main/java/modulo/matriculacion/VisualizadorMatriculas.java
@@ -14,6 +14,24 @@ public class VisualizadorMatriculas {
 
     public void mostrarMateriasDisponibles(Estudiante estudiante){
 
+        System.out.printf("%-20s","Materias");
+        System.out.println();
+        int indice=1;
+        System.out.println("Segunda Matricula");
+        for (Materia materia:estudiante.getCurriculum().obtenerMateriasReprobadas(estudiante)){
+            System.out.println(indice+"."+" "+materia.getNombre());
+            materiasDisponibles.add(materia);
+            indice++;
+        }
+        System.out.println("Materias Disponibles");
+        for (Materia materia:
+        estudiante.getCarrera().obtenerMateriasSinAprobar(estudiante)) {
+            if(!materia.isSegundaMatricula()){
+                System.out.println(indice+"."+" "+materia.getNombre());
+                materiasDisponibles.add(materia);
+            }
+            indice++;
+        }
     }
 
     public void mostrarHorariosDeMateria(Materia materia){
