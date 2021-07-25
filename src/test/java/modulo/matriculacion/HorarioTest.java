36c371938ac3f2b2337cf7b8dc020f476ec43b23
Davidaguirre99 <Davidaguirre99@users.noreply.github.com>
2021-07-25 01:56:56 -0500

    Se genera HorarioTest a partir de Horario y se implementan el setup y dos pruebas unitarias

---
+++ src/test/java/modulo/matriculacion/HorarioTest.java
@@ -0,0 +1,31 @@
+package modulo.matriculacion;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class HorarioTest {
+    Horario horario=null;
+    ArrayList<Integer> horasDia;
+    @Before
+    void setup(){
+        horasDia= new ArrayList<Integer>();
+        horasDia.add(0);
+        horasDia.add(1);
+        Horario horario = new Horario(4,horasDia);
+    }
+
+    @Test
+    public void given_horario_when_validarHorario_then_ok() {
+
+        assertTrue(horario.validarHorario());
+    }
+    @Test
+    public void given_horarioincorrecto_when_validarHorario_then_returnfalse(){
+        horario=new Horario(7,horasDia);
+        assertFalse(horario.validarHorario());
+    }
+}
